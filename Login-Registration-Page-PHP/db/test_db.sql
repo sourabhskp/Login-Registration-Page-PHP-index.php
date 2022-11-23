ef492dddbf1c67b4de59cd45f5f1a62287772844
sourabhskp <81424600+sourabhskp@users.noreply.github.com>
2022-11-23 23:34:48 +0530

    Add files via upload

---
+++ Login-Registration-Page-PHP/db/test_db.sql
@@ -0,0 +1,45 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+-- Database: `test_db`
+
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `user_name`, `password`, `name`) VALUES
+(3, 'ela', '202cb962ac59075b964b07152d234b70', 'Ela'),
+(4, 'elias', '202cb962ac59075b964b07152d234b70', 'elias');
+
+
+-- Indexes for dumped tables
+
+-- Indexes for table `users`
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+
